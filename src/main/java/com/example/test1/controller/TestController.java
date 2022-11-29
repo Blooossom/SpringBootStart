083d72e6e600ee63441f611cdb52b08a32d4a716
Blooossom <taegyeong19@gmail.com>
2022-11-29 16:16:14 +0900

    feat:Test Method added

--- src/main/java/com/example/test1/controller/TestController.java
+++ src/main/java/com/example/test1/controller/TestController.java
@@ -4,6 +4,8 @@ package com.example.test1.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
@@ -13,4 +15,40 @@ public class TestController {
         return "hello";
 
     }
+
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+
+
+    @GetMapping("hello-spring")
+    @ResponseBody
+    public String helloSpring(@RequestParam("name") String name){
+        return "hello"+name;
+    }//데이터를 그대로 내려줌
+
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloapi(@RequestParam("name")String name){
+        Hello hello  = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+
 }
---
+++ src/main/java/com/example/test1/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package com.example.test1.repository;
+
+import com.example.test1.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    //가져온 것이 null일수도 있지만
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}

6692c791686b9b5cd1e5c46c15ed9c76b8c0d73a
Blooossom <taegyeong19@gmail.com>
2022-11-29 16:55:11 +0900

    feat:MemberSerivce/Test method added

---
+++ src/main/java/com/example/test1/service/MemberService.java
@@ -0,0 +1,48 @@
+package com.example.test1.service;
+
+import com.example.test1.domain.Member;
+import com.example.test1.repository.MemberRepository;
+import com.example.test1.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+
+    private final MemberRepository memberRepository;
+    public MemberService(MemberRepository memberRepository){
+        this.memberRepository=memberRepository;
+    }
+    //memberRepository를  직접 생성해주는 것이 아니라 외부에서 넣어주도록 함
+
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+    public List<Member> findMember(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(member1 -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+        //회원가입 중복처리
+         /*
+        아래 코드를 줄이면 위처럼
+        Optional<Member> result = memberRepository.findByName(member.getName());
+
+        result.ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다");
+        });
+        */
+
+    }
+}
